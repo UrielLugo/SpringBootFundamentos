d73fbb1d46363f1933b4eda84252209d2e961a65
ulugo <ulugo@MacBook-Pro.local>
2021-09-12 17:39:33 -0500

    Inyeccion de dependencia 'Component'

---
+++ src/main/java/com/platzi/springboot/fundamentos/component/ComponentDependency.java
@@ -0,0 +1,5 @@
+package com.platzi.springboot.fundamentos.component;
+
+public interface ComponentDependency {
+    void saludar();
+}
---
+++ src/main/java/com/platzi/springboot/fundamentos/component/ComponentTwoImplement.java
@@ -0,0 +1,13 @@
+package com.platzi.springboot.fundamentos.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentTwoImplement implements ComponentDependency {
+
+    @Override
+    public void saludar() {
+        System.out.println("Hello World desde el componente dos");
+    }
+    
+}
